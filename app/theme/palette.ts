e0a7aa34c3265a3197a3f21834106747ff52dd1f
GhostMander <58628835+GhostMander@users.noreply.github.com>
2022-07-06 12:31:18 +0530

    First Commmit

---
+++ app/theme/palette.ts
@@ -0,0 +1,34 @@
+export const palette = {
+  black: "#1d1d1d",
+  white: "#ffffff",
+  offWhite: "#e6e6e6",
+  orange: "#FBA928",
+  orangeDarker: "#EB9918",
+  lightGrey: "#939AA4",
+  lighterGrey: "#CDD4DA",
+  angry: "#dd3333",
+  deepPurple: "#5D2555",
+}
+
+export const darkTheme = {
+  header: "#7F9172",
+  text: "#FFFFFC",
+  background: "#454545",
+  button: "#7A89C2",
+  textBox: "#959595",
+  textHighlight: "#AEC3B0",
+  textUnderline: "#AEAEAE",
+  amountColor: "#D1D1D1",
+  textBoxPlaceholder: "#C7C7C7",
+}
+
+export const lightTheme = {
+  header: "#7F9172",
+  text: "#FFFFFC",
+  background: "#FFFFFF",
+  button: "#7A89C2",
+  textBox: "#959595",
+  textHighlight: "#AEC3B0",
+  textUnderline: "#AEAEAE",
+  amountColor: "#D1D1D1",
+}
